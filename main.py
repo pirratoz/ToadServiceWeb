d472fbd02e2e10957ef7c234eecf7fb7f44f4b8d
PirraToZ <pirratoz@gmail.com>
2026-01-06 22:37:36 +0300

    feat(): add env loader

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from source.app.web import get_app
 from source.endpoints import (
     main_page,
 )
+from dotenv import load_dotenv
 
 
 def main() -> None:
+    load_dotenv()
     app = get_app()
     app.blueprint(main_page)
     app.run(host="127.0.0.1", port=8888, debug=True, single_process=True)
