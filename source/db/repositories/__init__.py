de4358f3e16e98542b2a5a65e4f4affff7ea076c
PirraToZ <pirratoz@gmail.com>
2026-01-09 21:37:36 +0300

    feat(): add task repository

--- source/db/repositories/__init__.py
+++ source/db/repositories/__init__.py
@@ -2,9 +2,11 @@ __all__ = [
     "BaseRepository",
     "SettingsRepository",
     "UserRepository",
+    "TaskRepository",
 ]
 
 
 from source.db.repositories.base_repository import BaseRepository
 from source.db.repositories.settings_repository import SettingsRepository
 from source.db.repositories.user_repository import UserRepository
+from source.db.repositories.task_repository import TaskRepository
---
+++ source/db/repositories/task_repository.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timezone
+
+from source.db.repositories.base_repository import BaseRepository
+from asyncpg import Connection
+
+
+class TaskRepository(BaseRepository):
+    def __init__(self, connection: Connection):
+        super().__init__(connection)
+
+    async def create_tasks(self, user_id: int) -> None:
+        sql = """
+        INSERT INTO tasks (user_id, task_type, next_run, turn, extra) VALUES
+            ($1, 'work', $2, FALSE, '{"type": null}'),
+            ($1, 'reward_clan', $2, FALSE, '{}'),
+            ($1, 'reward_marriage', $2, FALSE, '{}'),
+            ($1, 'eat_frog', $2, FALSE, '{}'),
+            ($1, 'eat_toad', $2, FALSE, '{}');
+        """
+        await self.connection.execute(sql, user_id, datetime.now(timezone.utc))
