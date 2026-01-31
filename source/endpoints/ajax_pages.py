0dc1a4def283e05297f9bac1c10d45b633df465f
PirraToZ <pirratoz@gmail.com>
2026-01-31 16:31:53 +0300

    feat(): add error

--- source/endpoints/ajax_pages.py
+++ source/endpoints/ajax_pages.py
@@ -215,6 +215,7 @@ async def set_telegram_turn(request: Request):
     except Exception as error:
         del storage.AuthInfoClass.clients[request.ctx.user_id]
         response.add_message("Что-то пошло не так...")
+        response.add_message(error)
         response.add_message("1. Перезагрузите страницу")
         response.add_message("2. Введите снова код телеграма!")
 
@@ -263,9 +264,10 @@ async def set_telegram_code(request: Request):
             response.set_running(not response.running)
             response.add_message("Попробуйте вход через код снова!")
             response.set_message_type(MessageType.WARNING)
-    except:
+    except Exception as error:
         del storage.AuthInfoClass.clients[request.ctx.user_id]
         response.add_message("Что-то пошло не так...")
+        response.add_message(error)
         response.add_message("1. Перезагрузите страницу")
         response.add_message("2. Введите снова код телеграма!")
     
